a3a5e1d125e320deab83a3087c99904219b28145
bao <bao-tran.cong-huyen-ton-nu.1@etsmtl.net>
2022-09-12 17:57:15 -0400

    Complete 3.13 and 3.14: Fetch and save data in the database

--- index.js
+++ index.js
@@ -1,47 +1,32 @@
+/// MODULES
+require('dotenv').config()
 const express = require('express')
 var morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
+/// CONFIGS
 const app = express()
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
 
+// Format logs
 morgan.token('body', (req, res) => {
   if (req.method === "POST") {
     return JSON.stringify(req.body)
   }
 });
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
-
+/// HTTP METHODS
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
+//TODO:
 app.get('/info', (request, response) => {
   const currentDate = new Date()
 
@@ -53,16 +38,13 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
 
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
+//TODO:
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
@@ -85,30 +67,27 @@ app.post('/api/persons', (request, response) => {
     return response.status(400).json({
       error: 'number missing'
     })
-  } else if (persons.find(p => p.name.toLowerCase() === body.name.toLowerCase())) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
   }
 
-  const person = {
+  const person = new Person({
     id: generateId(),
     name: body.name,
     number: body.number
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
-// Middleware that is used for catching requests 
-// made to non-existent routes
+/* Middleware that is used for catching requests 
+   made to non-existent routes */
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
-
 app.use(unknownEndpoint)
-const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
